ac653f9d29248b58f3a2b1cd69037c77a8e9456f
dc-pau <pauleenrasedechavez@gmail.com>
2026-08-07 17:08:14 +0800

    Main body for guide

---
+++ InventoryManagementSystem.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+
+public class InventoryManagementSystem{
+  public static Scanner input = new Scanner(System.in);
+  public static void main(String[] args){
+    System.out.println("Simple Inventory Management System by Ma. Angelica Pauleen R. De Chavez of C2A\n");
+
+    boolean exitProgram = false;
+
+    while(!exitProgram){
+      System.out.println("\n= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =");
+      System.out.println("                                          Menu Options");
+      System.out.println("= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =");
+
+      System.out.println("""
+
+                                  [1] Add Item                                     [6] Search Items
+                                  [2] Update Item                                  [7] Sort Items
+                                  [3] Remove Item                                  [8] Display Low Stock Items
+                                  [4] Display Items by Category                    [9] Exit
+                                  [5] Display All Items                
+                        """);
+      System.out.println("       - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -");
+
+      System.out.print("Enter Menu Choice: ");
+      int menu = input.nextInt();
+
+      switch(menu){
+        case 1:
+          //add item
+          break;
+        case 2:
+          //update item
+          break;
+        case 3:
+          //remove item
+          break;
+        case 4:
+          //display items by category
+          break;
+        case 5:
+          //display all items
+          break;
+        case 6:
+          //search item
+          break;
+        case 7:
+          //sort items
+          break;
+        case 8:
+          //display low stock items
+          break;
+        case 9:
+          //exit
+          exitProgram = true;
+          break;
+      }
+    }
+  }
+}
